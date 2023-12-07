9a889a6bab31e26cffcf459de9e6275d511c86e3
No-Sign-Of <hijikatashine@gmail.com>
2023-12-07 13:11:10 +0300

    Первая доработка

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,22 +3,16 @@ import requests
 import data
 
 
-# Функция для отправления запроса на создание нового заказа
+# Функция на отправление запроса на создание нового заказа
 def post_new_order(order_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
                          json=order_body,
                          headers=data.headers)
 
 
-track = post_new_order(data.order_body).json()["track"]
-
-
 # Функция на отправление запроса на получение заказа по номеру
-def get_order_by_track():
-    params_get_order = data.params.copy()
-    params_get_order["t"] = track
+def get_order_by_track(track):
+    current_params = data.params.copy()
+    current_params["t"] = track
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK_PATH,
-                        params=params_get_order)
-
-
-response_get_order = get_order_by_track()
+                        params=current_params)
